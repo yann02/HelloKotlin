09a3b5a0e2be60272ea5f54048f073f3a383769a
yingyanwu <1179858356@qq.com>
2020-05-27 21:32:02 +0800

    add 添加java和kotlin练习库(Add lib of java and kotlin.)

---
+++ libjava/src/main/java/com/example/libjava/MyClass.java
@@ -0,0 +1,7 @@
+package com.example.libjava;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Java.I have been alone.");
+    }
+}
---
+++ libkotlin/src/main/java/com/example/libkotlin/MyClass.kt
@@ -0,0 +1,12 @@
+package com.example.libkotlin
+
+public class MyClass {
+}
+
+/**
+ * For practice method of kotlin.
+ * Let method of main outside MyClass.
+ */
+fun main() {
+    println("It's great!")
+}
