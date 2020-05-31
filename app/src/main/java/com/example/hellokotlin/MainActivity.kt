af74df9e17debaf6b136eb4641e6dd91bf680324
yingyanwu <1179858356@qq.com>
2020-05-31 18:54:47 +0800

    添加隐式跳转到下一页(Add implicit intent to the next page.)

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.hellokotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var next: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        next = findViewById(R.id.btn_next)
+        next.setOnClickListener() {
+            val nextIntent = Intent().apply {
+                action = "123"
+                putExtra("abc", "Job")
+                type = "text/plain"
+            }
+            if (nextIntent.resolveActivity(packageManager) != null) {
+                startActivity(nextIntent)
+            }
+        }
     }
 }
