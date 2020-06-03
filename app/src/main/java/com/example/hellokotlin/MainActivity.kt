8ab4a5f3fa7c722ab4b03e962bd446eaadf4dbdd
yingyanwu <1179858356@qq.com>
2020-06-03 22:17:14 +0800

    add main.xml 首页添加菜单功能

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.hellokotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -22,5 +23,10 @@ class MainActivity : AppCompatActivity() {
                 startActivity(nextIntent)
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
     }
 }
